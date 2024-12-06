6e177fb2469581c744e429b698aee4225d35e3be
Charles Clarke <charles4clarke@gmail.com>
2024-12-06 11:51:48 -0700

    installed ws package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000', // Replace 4000 with your backend port if different
+      '/api': 'http://localhost:4000', 
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
     },
   },
 });
